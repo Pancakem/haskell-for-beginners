7e7f4612ee2a378904d35f090b9ba0066f7be9f2
Pancakem <pancakesdeath@protonmail.com>
2019-03-30 14:28:42 +0300

    complete chapter 2.1

--- problems/chapter_02/1_babys_first_functions.hs
+++ problems/chapter_02/1_babys_first_functions.hs
@@ -1,38 +1,54 @@
 -- define a function named avg that calculates the
 -- average of two numbers
-avg x y = undefined
+avg x y = (x + y) / 2
 
 -- use the avg function to calculate two different
 -- "averages" of the numbers 2, 3 and 4.
 --   The "averages" are 2.75 and 3.25
-avg1 = undefined
-avg2 = undefined
+avg1 = avg (avg 2 3) 4
+avg2 = avg (avg 4 3) 2
 
 -- recalculate the two numbers above using infix notation
-avg1_infix = undefined
-avg2_infix = undefined
+avg1_infix = (2 `avg` 3) `avg` 4
+avg2_infix = (4 `avg` 3) `avg` 2
 
 -- define a function that returns a boolean indicating
 -- whether or not an interger is a multiple of 5 *or* 3
 --   hint: 6 `mod` 3 is 0
 --         6 `mod` 5 is 1
-
+isMultipleOfFiveorThree x = 
+    x `mod` 3 == 0 || x `mod` 5 == 0
+  
 
 -- define a function that returns a boolean indicating
 -- whether or not a interger is a *not* multiple of 5 *and* 3
-
+isNotMultipleofFiveandThree x = 
+    not ((x `mod` 3 == 0) && (x `mod` 5 == 0))
 
 -- define a function that works just like the last one
 -- but uses a different boolean logic operator
 -- (or instead of and, or vice-versa)
 
+isNotMultipleofFiveandThree' x = 
+    (x `mod` 3 /= 0) || (x `mod` 5 /= 0)
+
 
 -- BONUS: define a function that works just like the
 -- last one but uses *NO* boolean logic operators at all
+isNotMultipleofFiveandThree'' x = 
+    (x `mod` 15 /= 0)
+
 
 -- define a function that checks to make sure the
 -- two (or three) functions above give the same answer for a
 -- provided number. It should return a string indicating
 -- to the user whether or not the functions matched
+checker x = 
+    if (isNotMultipleofFiveandThree' x) == (isNotMultipleofFiveandThree x) &&
+        isNotMultipleofFiveandThree'' x == isNotMultipleofFiveandThree x 
+    then
+        "Matched"
+    else
+        "Not matched"
 
 
