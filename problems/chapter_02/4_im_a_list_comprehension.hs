86c20d1448e51535fbf97d00b4239000a7c99d45
Pancakem <pancakesdeath@protonmail.com>
2019-04-23 17:16:32 +0300

    complete chapter 2.4

--- problems/chapter_02/4_im_a_list_comprehension.hs
+++ problems/chapter_02/4_im_a_list_comprehension.hs
@@ -1,10 +1,10 @@
 -- build a list of all the positive integers less than 100
 -- that are multiples of 3 *or* 5
-
+multiplesofthreeorfive = [ x | x <- [0..100], x `mod` 3 == 0 || x `mod` 5 == 0]
 
 -- write a function that takes two lists returns a new list
 -- by removing the contents of the second list from the first
-
+filternewList lis xs = [x | x <- lis, not (x `elem` xs)]
 
 -- write a function that accepts two lists of numbers and
 -- returns a list of each number in the first list repeating as much
@@ -18,7 +18,7 @@
 -- To avoid someone accidentally creating a GIGANTIC list, the function
 -- should ignore any lists longer than 20 elements
 --
-
+recklessRepeat lis xs = [ replicate d x | x <- lis, d <- xs, length lis <= 20 ]
 
 -- write a function that censors every string in a list by replacing
 -- the vowels in them with *
@@ -26,4 +26,5 @@
 -- For good measure, it should remove the string "dingleberry" from
 -- the list, if it appears
 --
-
+censorString str = [[ if char `elem` "aeiouAEIOU" then '*' else char | char <- strs ] 
+                        | strs <- str, strs /= "dingleberry" ]
