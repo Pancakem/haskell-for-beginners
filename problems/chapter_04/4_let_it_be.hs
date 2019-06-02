8024bcd04f70f5ffd16eaf77c119ae092cb5759a
Pancakem <pancakesdeath@protonmail.com>
2019-06-02 15:05:12 +0300

    complete chapter 4.4

--- problems/chapter_04/4_let_it_be.hs
+++ problems/chapter_04/4_let_it_be.hs
@@ -4,7 +4,13 @@
 --    (be sure to use *let* to make your function
 --     more readable)
 bigTriangle :: Float -> Float -> Bool
-bigTriangle length width = undefined
+bigTriangle length width = 
+    let 
+        hypoteneuse = sqrt (length*length + width*width)
+
+        ret = hypoteneuse > 10
+    in
+    ret
 
 
 -- Write a function that constructs a rect from a
@@ -14,19 +20,32 @@ bigTriangle length width = undefined
 -- matching skills to write the most readable
 -- function you can.
 listToRect :: [Integer] -> (Integer, Integer)
-listToRect = undefined
+listToRect xs = 
+    let
+        [length, width] = xs
+    in
+    (length, width)
 
 
 -- Write a function that calculates the area of
 -- each rectangle in a list. Write two versions
 -- of it, one with with let *outside* the list,
 -- and one with let *inside* the list.
+calcAreas :: [(Integer, Integer)] -> [Integer]
+calcAreas xs = 
+    let
+        area (l,w) = l*w
+    in
+    [area rect | rect <- xs]
 
+calcAreas' :: [(Integer, Integer)] -> [Integer]
+calcAreas' xs = 
+    [area rect | rect <- xs , let area (l,w) = l*w]
 
 -- Write a function to calculate the area of a
 -- square donut based on its size and thickness.
 -- For instance, the square below has size 5,
--- thinkness 1, area 16.
+-- thickness 1, area 16.
 --
 --                   █████
 --                   █   █
@@ -35,5 +54,11 @@ listToRect = undefined
 --                   █████
 --
 donutArea :: Num a => a -> a -> a
-donutArea size thickness = undefined
+donutArea size thickness = 
+    let
+        innersize = size - thickness*2
+        innerarea = innersize*innersize
+        outerarea = size*size
+    in
+    outerarea - innerarea
 
