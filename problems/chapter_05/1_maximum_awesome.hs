1ad146488ad98d46b27bd993d139119e678da84a
Pancakem <pancakesdeath@protonmail.com>
2019-10-23 16:09:24 +0300

    complete 5.1

--- problems/chapter_05/1_maximum_awesome.hs
+++ problems/chapter_05/1_maximum_awesome.hs
@@ -1,12 +1,19 @@
 -- Define a list of all Fibonacci numbers
 
 fib :: Num a => [a]
-fib = undefined
+fib = 0 : 1 : zipWith (+) fib (tail fib)
 
 -- Implement a recursive function to find
 -- the maximum element in a list of
 -- pairs of elements.
 
 maxPairElement :: Ord a => [(a,a)] -> a
-maxPairElement = undefined
+maxPairElement [] = error "Emoty list"
+
+maxPairElement [(x,y)]
+    | x >= y = x
+    | y >= x = y
+
+maxPairElement ((x,y):xs) = max bigger (maxPairElement xs)
+    where bigger = max x y
 
